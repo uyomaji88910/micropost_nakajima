058317eea8669d097db5df52e65b5b2d8225569b
ryo.nakajima <ryo.nakajima@rakuten.com>
2018-06-07 04:58:44 +0000

    user login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/'; // Mod by  Ryo Nakajima 06/07 7.2
 
     /**
      * Create a new controller instance.
