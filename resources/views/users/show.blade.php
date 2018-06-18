af003b1b9140b6d32ea931e3cbdfb54e69cdca60
ryo.nakajima <ryo.nakajima@rakuten.com>
2018-06-18 06:46:17 +0000

    follow, unfollow

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -11,18 +11,19 @@
                     <img class="media-object img-rounded img-responsive" src="{{ Gravatar::src($user->email, 500) }}" alt="">
                 </div>
             </div>
+    @include('user_follow.follow_button', ['user' => $user])
         </aside>
         <div class="col-xs-8">
             <ul class="nav nav-tabs nav-justified">
-                <li role="presentation" class="{{ Request::is('users/' . $user->id) ? 'active' : '' }}"><a href="{{ route('users.show', ['id' => $user->id]) }}">TimeLine <span class="badge">{{ $count_microposts }}</span></a></li>
-                <li><a href="#">Followings</a></li>
-                <li><a href="#">Followers</a></li>
+               <li role="presentation" class="{{ Request::is('users/' . $user->id) ? 'active' : '' }}"><a href="{{ route('users.show', ['id' => $user->id]) }}">TimeLine <span class="badge">{{ $count_microposts }}</span></a></li>
+               <li role="presentation" class="{{ Request::is('users/*/followings') ? 'active' : '' }}"><a href="{{ route('users.followings', ['id' => $user->id]) }}">Followings <span class="badge">{{ $count_followings }}</span></a></li>
+               <li role="presentation" class="{{ Request::is('users/*/followers') ? 'active' : '' }}"><a href="{{ route('users.followers', ['id' => $user->id]) }}">Followers <span class="badge">{{ $count_followers }}</span></a></li>
             </ul>
-    @if (Auth::user()->id == $user->id)
-    {!! Form::open(['route' => 'microposts.store']) !!}
+     @if (Auth::id() == $user->id)
+         {!! Form::open(['route' => 'microposts.store']) !!}
                       <div class="form-group">
                       {!! Form::textarea('content', old('content'), ['class' => 'form-control', 'rows' => '2']) !!}
-                      {!! Form::submit('Post', ['class' => 'btn btn-primary btn-block']) !!}
+                          {!! Form::submit('Post', ['class' => 'btn btn-primary btn-block']) !!}
                       </div>
                       {!! Form::close() !!}
             @endif
